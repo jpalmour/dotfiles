9e2a747ccede9a1c75027b74bb8d93bc971aa2c2
Joseph Palmour <jpalmour@gmail.com>
2018-03-06 19:31:41 -0500

    enable one line install

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,6 @@
 
 cd "$(dirname -- "$0")"
 
-source ~/.secrets
-
 git pull origin master
 git submodule update --init --recursive
 
@@ -16,6 +14,16 @@ function doIt() {
 	      -avh\
 		  --no-perms\
 		  . ~;
+
+    # install brew if not already installed
+	command -v brew > /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+	# install zsh if not already installed
+	command -v zsh > /dev/null || brew install zsh
+
+	# install oh-my-zsh if not already installed
+	[[ -d $HOME/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 	source ~/.zshenv;
 	source ~/.zshrc;
 
@@ -24,6 +32,9 @@ function doIt() {
 
 	# to avoid errors when pulling private repos using go get
 	git config --global url."https://${JPALMOUR_GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+
+	# install things managed by brew
+	~/brew.sh
 	
 }
 
