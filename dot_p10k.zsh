f21b60d12a953431e0b2297f51e1d227b2c82f11
Joseph Palmour <jpalmour@salesforce.com>
2025-11-15 12:28:04 -0500

    initial attempt

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -98,9 +98,9 @@
   typeset -g POWERLEVEL9K_BACKGROUND=240
 
   # Separator between same-color segments on the left.
-  typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%246F\uE0B1'
+  typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%44F\uE0B1'
   # Separator between same-color segments on the right.
-  typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='%246F\uE0B3'
+  typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='%44F\uE0B3'
   # Separator between different-color segments on the left.
   typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B0'
   # Separator between different-color segments on the right.
@@ -151,7 +151,7 @@
   ##################################[ dir: current directory ]##################################
   # Default current directory color (cyan #26C6DA).
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=44
-  # Truncate directory to max 20 chars (matching OMP theme).
+  # Show at most the last two path segments (matching OMP theme).
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER='...'
@@ -201,13 +201,13 @@
   # respectively.
   typeset -g POWERLEVEL9K_DIR_TRUNCATE_BEFORE_MARKER=false
   # Don't shorten this many last directory segments. They are anchors.
-  typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+  typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
   # Shorten directory if it's longer than this even if there is space for it. The value can
   # be either absolute (e.g., '80') or a percentage of terminal width (e.g, '50%'). If empty,
   # directory will be shortened only when prompt doesn't fit or when other parameters demand it
   # (see POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS and POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS_PCT below).
   # If set to `0`, directory will always be shortened to its minimum length.
-  typeset -g POWERLEVEL9K_DIR_MAX_LENGTH=20
+  typeset -g POWERLEVEL9K_DIR_MAX_LENGTH=0
   # When `dir` segment is on the last prompt line, try to shorten it enough to leave at least this
   # many columns for typing commands.
   typeset -g POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS=40
@@ -277,10 +277,18 @@
   # parameter. For example, if POWERLEVEL9K_DIR_WORK_NOT_WRITABLE_FOREGROUND is not set, it falls
   # back to POWERLEVEL9K_DIR_FOREGROUND.
   #
-  typeset -g POWERLEVEL9K_DIR_CLASSES=()
+  typeset -g POWERLEVEL9K_DIR_CLASSES=(
+    '~/repos/github.com(|/*)' GITHUB ''
+    '*'                     DEFAULT ''
+  )
 
   # Custom prefix.
-  # typeset -g POWERLEVEL9K_DIR_PREFIX='%248Fin '
+  # Icon for directories under ~/repos/github.com.
+  typeset -g POWERLEVEL9K_DIR_GITHUB_VISUAL_IDENTIFIER_EXPANSION=''
+  typeset -g POWERLEVEL9K_DIR_GITHUB_FOREGROUND=44
+  typeset -g POWERLEVEL9K_DIR_GITHUB_SHORTENED_FOREGROUND=44
+  typeset -g POWERLEVEL9K_DIR_GITHUB_ANCHOR_FOREGROUND=44
+
 
   #####################################[ vcs: git status ]######################################
   # Branch icon. Set this parameter to '\UE0A0 ' for the popular Powerline branch icon.
@@ -328,11 +336,31 @@
 
     if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
       local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
-      # If local branch name is at most 15 characters long, show it in full (matching OMP theme).
-      # Otherwise show the first 15 with ...
-      # Tip: To always show local branch name in full without truncation, delete the next line.
-      (( $#branch > 15 )) && branch="${branch[1,15]}..."  # <-- this line
-      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
+      local mapped=$branch rest
+      case $branch in
+        feat/*)
+          rest=${branch#feat/}
+          mapped=$'\uF135/'$rest
+          ;;
+        feature/*)
+          rest=${branch#feature/}
+          mapped=$'\uF135/'$rest
+          ;;
+        bug/*)
+          rest=${branch#bug/}
+          mapped=$'\uF188/'$rest
+          ;;
+        bugfix/*)
+          rest=${branch#bugfix/}
+          mapped=$'\uF188/'$rest
+          ;;
+        experimental/*)
+          rest=${branch#experimental/}
+          mapped=$'\uF0C3/'$rest
+          ;;
+      esac
+      (( $#mapped > 30 )) && mapped="${mapped[1,30]}…"
+      res+="${clean}${mapped//\%/%%}"
     fi
 
     if [[ -n $VCS_STATUS_TAG
@@ -379,16 +407,18 @@
     (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
     # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
     (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
-    # *42 if have stashes.
-    (( VCS_STATUS_STASHES        )) && res+=" ${clean}*${VCS_STATUS_STASHES}"
+    #  42 if have stashes.
+    (( VCS_STATUS_STASHES        )) && res+=" ${clean} ${VCS_STATUS_STASHES}"
     # 'merge' if the repo is in an unusual state.
     [[ -n $VCS_STATUS_ACTION     ]] && res+=" ${conflicted}${VCS_STATUS_ACTION}"
     # ~42 if have merge conflicts.
     (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
-    # +42 if have staged changes.
-    (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
-    # !42 if have unstaged changes.
-    (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+    # Show staged/unstaged changes similar to OMP.
+    (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified} ${VCS_STATUS_NUM_UNSTAGED}"
+    if (( VCS_STATUS_NUM_UNSTAGED && VCS_STATUS_NUM_STAGED )); then
+      res+=" ${meta}|"
+    fi
+    (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified} ${VCS_STATUS_NUM_STAGED}"
     # ?42 if have untracked files. It's really a question mark, your font isn't broken.
     # See POWERLEVEL9K_VCS_UNTRACKED_ICON above if you want to use a different icon.
     # Remove the next line if you don't want to see untracked files at all.
@@ -432,7 +462,7 @@
   # Custom icon.
   typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_EXPANSION=
   # Custom prefix (subsegment separator in cyan).
-  typeset -g POWERLEVEL9K_VCS_PREFIX='%44F\uE0B1 '
+  typeset -g POWERLEVEL9K_VCS_PREFIX=
 
   # Show status of repositories of these types. You can add svn and/or hg if you are
   # using them. If you do, your prompt may become slow even when your current directory
